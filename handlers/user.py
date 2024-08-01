ab6ce7919262667b33fd090f9ec2dc7704ec69e5
Almaz Galimov <almazwork@gmail.com>
2024-08-01 18:31:53 +0500

    add saving fsm states to nats storage

--- handlers/user.py
+++ handlers/user.py
@@ -1,7 +1,10 @@
 from aiogram import F, Router, html
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command, StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from fluentogram import TranslatorRunner
+
+from states.states import NatsTestSG
 
 user_router = Router()
 
@@ -22,7 +25,41 @@ async def process_start_command(message: Message, i18n: TranslatorRunner):
     )
 
 
+# Хендлер срабатывает на команду /update
+@user_router.message(Command(commands='update'))
+async def process_update_command(message: Message, i18n: TranslatorRunner, state: FSMContext):
+    # Отправляем пользователю сообщение с предложением прислать любой текст
+    await message.answer(text=i18n.send.text())
+    # Устанавливаем состояние ожидания ввода текста
+    await state.set_state(NatsTestSG.enter_text)
+
+
+# Хендлер срабатывает на команду /read
+@user_router.message(Command(commands='read'))
+async def process_read_command(message: Message, state: FSMContext):
+    # Получаем FSM data
+    data = await state.get_data()
+    # Отправляем в телеграм-клиент строковое представление FSM data
+    await message.answer(text=str(data))
+
+
+# Хендлер срабатывает на любой текст в состоянии 'NatsTestSG.enter_text'
+@user_router.message(F.text, StateFilter(NatsTestSG.enter_text))
+async def process_enter_text(message: Message, i18n: TranslatorRunner, state: FSMContext):
+    # Обновляем FSM data
+    await state.update_data(text=message.text)
+    await message.answer(text=i18n.successfully.saved())
+    await state.set_state()
+
+
+# Хендлер срабатывает на любое нетекстовое сообщение в состоянии 'NatsTestSG.enter_text'
+@user_router.message(StateFilter(NatsTestSG.enter_text))
+async def process_any_message(message: Message, i18n: TranslatorRunner, state: FSMContext):
+    await message.answer(text=i18n.text.only())
+
+
 # Хендлер срабатывает на нажатие инлайн кнопки
 @user_router.callback_query(F.data == 'button_pressed')
 async def process_callback_query(callback: CallbackQuery, i18n: TranslatorRunner):
     await callback.answer(text=i18n.button.pressed())
+
